12748729d9a6a3bb7d7f87f6c6c6ef19919dbdee
Tom <bernard5150@gmail.com>
2016-10-16 18:44:40 -0700

    first commit

---
+++ MathTools.js
@@ -0,0 +1,15 @@
+
+ function average(numberarray){
+
+
+    var total = 0;
+
+    for(var i = 0; i < arrayP.length; i++){
+      this.total += arrayP[i];
+    }
+
+
+
+
+    return this.total/this.arrayP.length;
+  }
---
+++ routes/index.js
@@ -0,0 +1,111 @@
+var express = require('express');
+//var MathTools = require('../MathTools.js');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/average', function(req, res, next){
+  //get the data from the req object json array
+  var numberArray = JSON.parse(req.body.numberArray);
+  var average = 0;
+  var total = 0;
+  console.log(numberArray + " ");
+
+  for (var i = 0; i<numberArray.length; i++)
+  {
+    total += numberArray[i];
+    console.log(numberArray[i] + "is the value at index" + i);
+  }
+
+  console.log("numberArray length is ->" + numberArray.length);
+  console.log(numberArray.length + "is a lenght");
+  console.log(total + "is the total");
+  average = total / numberArray.length;
+  console.log(average +"this is the average");
+
+  res.json({ "average" : average });
+
+  //this line will grab the array
+
+  //call the function called average which is located in
+
+  //antoher file and pass the data into as a parameter
+  //get the return value form the function
+  //return the value which is the JSON object, to the user via the res
+  //object
+
+  //do this for all 6 functions
+
+});
+
+
+router.post('/largenumber', function(req, res, next){
+  //get the data from the req object json array
+  //call the function called average which is located in
+  //antoher file and pass the data into as a parameter
+  //get the return value form the function
+  //return the value which is the JSON object, to the user via the res
+  //object
+  //do this for all 6 functions
+
+
+});
+
+
+router.post('/evennumber', function(req, res, next){
+  //get the data from the req object json array
+  //call the function called average which is located in
+  //antoher file and pass the data into as a parameter
+  //get the return value form the function
+  //return the value which is the JSON object, to the user via the res
+  //object
+  //do this for all 6 functions
+
+
+});
+
+
+router.post('/alleven', function(req, res, next){
+  //get the data from the req object json array
+  //call the function called average which is located in
+  //antoher file and pass the data into as a parameter
+  //get the return value form the function
+  //return the value which is the JSON object, to the user via the res
+  //object
+  //do this for all 6 functions
+
+
+});
+
+
+router.post('/stringsearch', function(req, res, next){
+  //get the data from the req object json array
+  //call the function called average which is located in
+  //antoher file and pass the data into as a parameter
+  //get the return value form the function
+  //return the value which is the JSON object, to the user via the res
+  //object
+  //do this for all 6 functions
+
+
+});
+
+
+router.post('/stringsearchtwice', function(req, res, next){
+  //get the data from the req object json array
+  //call the function called average which is located in
+  //antoher file and pass the data into as a parameter
+  //get the return value form the function
+  //return the value which is the JSON object, to the user via the res
+  //object
+  //do this for all 6 functions
+
+
+});
+
+
+
+module.exports = router;
