c449986cd1957e5633e2f449695a8fac0c558950
Tom <bernard5150@gmail.com>
2016-10-16 22:06:16 -0700

    Added posts for large numbers, max, and even

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.post('/average', function(req, res, next){
   var numberArray = JSON.parse(req.body.numberArray);
   var average = 0;
   var total = 0;
-  console.log(numberArray + " ");
+//  console.log(numberArray + " ");
 
   for (var i = 0; i<numberArray.length; i++)
   {
@@ -28,41 +28,46 @@ router.post('/average', function(req, res, next){
 
   res.json({ "average" : average });
 
-  //this line will grab the array
 
-  //call the function called average which is located in
-
-  //antoher file and pass the data into as a parameter
-  //get the return value form the function
-  //return the value which is the JSON object, to the user via the res
-  //object
-
-  //do this for all 6 functions
 
 });
 
 
 router.post('/largenumber', function(req, res, next){
-  //get the data from the req object json array
-  //call the function called average which is located in
-  //antoher file and pass the data into as a parameter
-  //get the return value form the function
-  //return the value which is the JSON object, to the user via the res
-  //object
-  //do this for all 6 functions
+  var max = 0;
+  var numberArray = JSON.parse(req.body.numberArray);
 
+  for (var i = 0; i<numberArray.length; i++)
+  {
+    if (max < numberArray[i]){
+      max = numberArray[i];
+    }
+  }
+  res.json({ "max" : max });
 
 });
 
 
 router.post('/evennumber', function(req, res, next){
-  //get the data from the req object json array
-  //call the function called average which is located in
-  //antoher file and pass the data into as a parameter
-  //get the return value form the function
-  //return the value which is the JSON object, to the user via the res
-  //object
-  //do this for all 6 functions
+  var even = 1;
+  var numberArray = JSON.parse(req.body.numberArray);
+
+
+  for (var i = 0; i<numberArray.length; i++)
+  {
+
+    if (numberArray[i] % 2 == 0)
+    {
+      console.log("true");
+      return true;
+    }
+    else
+    {
+      console.log("false");
+      return false;
+    }
+
+  }
 
 
 });
